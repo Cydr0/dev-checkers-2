0523f6ec0320dd481ec063cb23f760d266275d60
aporrt <adenporter97@gmail.com>
2019-08-22 12:20:52 +0930

    Added Aden

--- Assets/People.cs
+++ Assets/People.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class People : MonoBehaviour {
 
     private string lachie = "mcin0283";
+    private string adenpo = "port0212";
 
     // Start is called before the first frame update
     void Start() {
