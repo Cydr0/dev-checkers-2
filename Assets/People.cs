7ecacaf0a3ad1a734dd09d184ebc1ef31c332480
lesk0014 <julz3.14159@gmail.com>
2019-08-23 13:52:09 +0930

    Added lesk0014

--- Assets/People.cs
+++ Assets/People.cs
@@ -5,11 +5,9 @@ using UnityEngine;
 public class People : MonoBehaviour {
 
     private string lachie = "mcin0283";
-<<<<<<< HEAD
     private string aidenJ = "jaco0160";
-=======
     private string adenpo = "port0212";
->>>>>>> 0523f6ec0320dd481ec063cb23f760d266275d60
+    private string julian = "lesk0014";
 
     // Start is called before the first frame update
     void Start() {
