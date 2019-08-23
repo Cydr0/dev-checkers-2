8811b881ac79a1dba11682d572a19b40b8e22afb
AidenJ-jaco0160 <aidenwjacobs@hotmail.com>
2019-08-23 13:05:27 +0930

    Added AidenJ

--- Assets/People.cs
+++ Assets/People.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class People : MonoBehaviour {
 
     private string lachie = "mcin0283";
+    private string aidenJ = "jaco0160";
 
     // Start is called before the first frame update
     void Start() {
