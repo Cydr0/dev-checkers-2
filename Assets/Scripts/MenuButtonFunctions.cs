33453ca3288247906d1ce9d80804897471ee0bb3
AidenJ-jaco0160 <aidenwjacobs@hotmail.com>
2019-09-10 09:34:26 +0930

    Created Simple Menu Template
    Created a menu template to be implemented further after designed
    inlcudes:
     - Game Title
     - Start Button
     - Quit Button

---
+++ Assets/Scripts/MenuButtonFunctions.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuButtonFunctions : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start(){
+        
+    }
+
+    // Update is called once per frame
+    void Update(){
+        
+    }
+
+    public void playButton()
+    {
+        SceneManager.LoadScene("TerrainTest", LoadSceneMode.Additive);
+    }
+
+    public void quitButton()
+    {
+        Application.Quit();
+    }
+}
